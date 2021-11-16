efa77a28082325d72c2facfdc62790b0fb17ed73
muddarla-nidhish <77198228+muddarla-nidhish@users.noreply.github.com>
2021-11-16 16:50:48 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+
+}
+function setup(){
+canvas=createCanvas(500,500);
+canvas.center();
+background("white");
+}
+function draw(){
+
+}
+function clear(){
+    background("white");
+}
